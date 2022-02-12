604be45d141a3ed445216fb3b6895cb391a14a92
Pedro Lima <pedrohblima03@gmail.com>
2022-02-12 20:12:19 -0300

    baseURL from axios

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,7 +3,7 @@ import axios from "axios"
 const getService = async () => {
   return await axios({
     method: "GET",
-    url: 'http://localhost:3030/'
+    baseURL: 'http://localhost:3030/'
   })
     .then(response => response)
     .catch(err => console.log(err))
